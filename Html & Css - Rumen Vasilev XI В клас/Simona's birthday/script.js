0961d2c4a277fc3210754ea5ac17df083ad658a0
Rumen Vasilev <rumenstvass@gmail.com>
2024-06-20 09:33:12 +0300

    Adding my school projects to the repository

---
+++ Html & Css - Rumen Vasilev XI В клас/Simona's birthday/script.js
@@ -0,0 +1,7 @@
+// function hoveringOverImage() {
+//     const imageElement = document.getElementsByClassName('media-gift-cover');
+
+//     for (const image of imageElement) {
+//         image.src = 'Birthday Gift Site/happy.webp';
+//     }
+// }
